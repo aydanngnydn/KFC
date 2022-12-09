e41c9bd751d1b54faf2d5a2bc2ef875a9a8df0cc
ouzayb <ouzayb@gmail.com>
2022-12-10 02:35:54 +0300

    holdable and moveable skeleton

---
+++ Assets/Scripts/Holdable.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Holdable : MonoBehaviour
+{
+	private bool mouseOver = false;
+	
+	private void OnMouseOver()
+	{
+		OnMouseHover();
+	}
+
+	private void OnMouseEnter()
+	{
+		mouseOver = true;
+	}
+
+	private void OnMouseExit()
+	{
+		mouseOver = false;
+	}
+
+	protected void OnMouseDown()
+	{
+		if (!mouseOver) return;
+
+		
+		if (Input.GetMouseButtonDown(0))
+		{
+			OnLeftMouseDown();
+		}
+		else if(Input.GetMouseButtonDown(1))
+		{
+			OnRightMouseDown();
+		}
+	}
+
+	private void OnMouseUp()
+	{
+		if (!mouseOver) return;
+
+		
+		if (Input.GetMouseButtonUp(0))
+		{
+			OnLeftMouseUp();
+		}
+		else if(Input.GetMouseButtonUp(1))
+		{
+			OnRightMouseUp();
+		}
+	}
+
+	private void OnMouseDrag()
+	{
+		OnMouseDragging();
+	}
+
+	protected abstract void OnMouseHover();
+	protected abstract void OnLeftMouseDown();
+	protected abstract void OnRightMouseDown();
+	protected abstract void OnLeftMouseUp();
+	protected abstract void OnRightMouseUp();
+	protected abstract void OnMouseDragging();
+}
---
+++ Assets/Scripts/Moveable.cs
@@ -0,0 +1,26 @@
+﻿public class Moveable: Holdable
+{
+	protected override void OnMouseHover()
+	{
+	}
+
+	protected override void OnLeftMouseDown()
+	{
+	}
+
+	protected override void OnRightMouseDown()
+	{
+	}
+
+	protected override void OnLeftMouseUp()
+	{
+	}
+
+	protected override void OnRightMouseUp()
+	{
+	}
+
+	protected override void OnMouseDragging()
+	{
+	}
+}
