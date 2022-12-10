0eae402a74fa665686243a9e5e0e5ba6aaf3192a
aydanngnydn <aydangunaydin@hotmail.com>
2022-12-10 03:45:27 +0300

    movement update

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float speed;
-    [SerializeField] private float direction;
     [SerializeField] private float typeChangeTime;
     private bool move = true, layDown = false;
+    private float xDir, yDir;
 
     private void Start()
     {
@@ -19,7 +22,7 @@ public class Movement : MonoBehaviour
     {
         if (move && !layDown)
         {
-            transform.Translate(Vector3.up * (direction * speed * Time.deltaTime));
+            transform.Translate(new Vector3(xDir, yDir, 0) * (speed * Time.deltaTime));
         }
         else if (!move && layDown)
         {
@@ -29,9 +32,10 @@ public class Movement : MonoBehaviour
 
     private IEnumerator MovementType()
     {
+        xDir = Random.Range(-1f, 1f); yDir = 1 - Mathf.Abs(xDir);
         move = true; layDown = false;
         yield return new WaitForSeconds(typeChangeTime);
         layDown = true; move = false;
-
+        yield return new WaitForSeconds(typeChangeTime);
     }
 }
