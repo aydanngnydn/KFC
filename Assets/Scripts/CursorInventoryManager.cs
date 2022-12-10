d7d267170bf4fbfb7c5c3d5350515465ad9d96c4
ouzayb <ouzayb@gmail.com>
2022-12-10 12:10:18 +0300

    pen and Cursor inventory manager

---
+++ Assets/Scripts/CursorInventoryManager.cs
@@ -0,0 +1,3 @@
+﻿public class CursorInventoryManager: Singleton<CursorInventoryManager>
+{
+}
---
+++ Assets/Scripts/Pen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Pen:Holdable
+{
+	public List<Chicken> chickens = new List<Chicken>();
+	public List<Egg> eggs = new List<Egg>();
+	protected override void OnMouseHover()
+	{
+		throw new NotImplementedException();
+	}
+
+	protected override void OnLeftMouseDown()
+	{
+		throw new NotImplementedException();
+	}
+
+	protected override void OnRightMouseDown()
+	{
+		throw new NotImplementedException();
+	}
+
+	protected override void OnLeftMouseUp()
+	{
+		throw new NotImplementedException();
+	}
+
+	protected override void OnRightMouseUp()
+	{
+		throw new NotImplementedException();
+	}
+
+	protected override void OnMouseDragging()
+	{
+		throw new NotImplementedException();
+	}
+}
