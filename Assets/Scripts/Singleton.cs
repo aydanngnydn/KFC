47c219ded65fad998075805842b4ccf34e181095
ouzayb <ouzayb@gmail.com>
2022-12-10 12:10:02 +0300

    singleton implementation

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	public static T I;
+	protected virtual void Awake()
+	{
+		if (I == null)
+		{
+			I = this as T;
+		}
+
+		if (I != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
