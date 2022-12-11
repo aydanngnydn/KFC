81601ba816cfca9a5446c64fd0e85435da993ea7
ouzayb <ouzayb@gmail.com>
2022-12-11 17:56:56 +0300

    chickens animations

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,22 +10,29 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float maxTime, minTime;
-    private bool move = true, layDown = false, aydo = false, hovered = false;
+    private bool move = true;
     private float xDir, yDir;
     private Vector3 dirVector;
-    public bool stop = false;
+    public bool stop = true;
     [SerializeField] private float checkDist = .5f;
     [SerializeField] private LayerMask wallLayerMask;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
 
+    private bool facingLeft = true;
     private void Start()
     {
+        if (!_animator)
+            _animator = GetComponent<Animator>();
+        if (!_spriteRenderer)
+            _spriteRenderer = GetComponent<SpriteRenderer>();
         StartCoroutine(MovementType());
     }
 
     private void Update()
     {
         if(stop) return;
-        if (move && !layDown)
+        if (move)
         {
             RaycastHit2D wallInfo = Physics2D.Raycast(transform.position, dirVector, checkDist,wallLayerMask);
 
@@ -40,42 +47,53 @@ public class Movement : MonoBehaviour
                 dirVector = new Vector3(dirVector.x, -dirVector.y, dirVector.z);
             }
 
+            switch (facingLeft)
+            {
+                case true when dirVector.x > 0:
+                    facingLeft = false;
+                    _spriteRenderer.flipX = true;
+                    break;
+                case false when dirVector.x < 0:
+                    facingLeft = true;
+                    _spriteRenderer.flipX = false;
+                    break;
+            }
+
             Debug.DrawLine(transform.position,transform.position + dirVector * checkDist,Color.green);
             transform.Translate(dirVector * (speed * Time.deltaTime));
-        }
-        else if (!move && layDown)
-        {
-            //animplay
         }
     }
 
     private IEnumerator MovementType()
     {
-        int type = Random.Range(1, 3);
         while (true)
         {
+            int type = Random.Range(1, 5);
             float typeChangeTime = Random.Range(minTime, maxTime);
-            while (type == 1 && !hovered)
+            if (type < 3)
             {
                 xDir = Random.Range(-1f, 1f);
                 yDir = Random.Range(-1f, 1f);
                 dirVector = Vector3.Normalize(new Vector3(xDir, yDir, 0));
-                move = true; layDown = false;
-                yield return new WaitForSeconds(typeChangeTime);
-                type = Random.Range(1, 3);
+                move = true;
+                _animator.SetTrigger("Move");
+
             }
 
-            while (type == 2)
+            if (type == 3)
             {
-                layDown = true; move = false;
-                yield return new WaitForSeconds(typeChangeTime);
-                type = Random.Range(1, 3);
+                move = false;
+                _animator.SetTrigger("Cluck");
             }
-
-            if (aydo)
+            
+            if (type == 4)
             {
-                break;
+                move = false;
+                _animator.SetTrigger("Idle");
             }
+            
+            
+            yield return new WaitForSeconds(typeChangeTime);
         }
     }
 }
